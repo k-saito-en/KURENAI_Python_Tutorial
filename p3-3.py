e45bc71a0b017f2473fc9c8cd57d123dfbe6feaa
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-09-11 20:41:11 +0900

    :+1: #3 演習3-4

---
+++ p3-3.py
@@ -0,0 +1,13 @@
+# kakaku = 1000
+# nebikiritsu= 15
+# kakaku = Kakaku*(100-nebikiritsu)/100 
+# print(kakaku)
+
+# 変数 nebikimae_kakaku に整数 1000 を格納
+nebikimae_kakaku = 1000
+# 変数 nebikiritsu に整数 15 を格納
+nebikiritsu= 15
+# 割引を適用した kakaku を計算
+kakaku = nebikimae_kakaku*(100-nebikiritsu)/100 
+# print関数で kakaku を表示
+print(kakaku)
--- p3-4.py
+++ p3-4.py
@@ -1,13 +1,9 @@
-# kakaku = 1000
-# nebikiritsu= 15
-# kakaku = Kakaku*(100-nebikiritsu)/100 
-# print(kakaku)
-
-# 変数 nebikimae_kakaku に整数 1000 を格納
-nebikimae_kakaku = 1000
-# 変数 nebikiritsu に整数 15 を格納
-nebikiritsu= 15
-# 割引を適用した kakaku を計算
-kakaku = nebikimae_kakaku*(100-nebikiritsu)/100 
-# print関数で kakaku を表示
-print(kakaku)
+a=1
+b = 1/2
+c = "ABC"
+print(a) 
+print(b) 
+print(c) 
+print(type(a)) 
+print(type(b)) 
+print(type(c))
