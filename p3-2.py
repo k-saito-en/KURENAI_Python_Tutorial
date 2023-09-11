0eb0ca9541a2f50a770e153c9a03b061e2b944f2
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-09-11 20:08:18 +0900

    :+1: issue#3 演習3-2

---
+++ p3-2.py
@@ -0,0 +1,16 @@
+print(1 + 2)
+
+# 演習3-2
+sum_two_one = 2 + 1
+sum_two_one
+print(sum_two_one)
+
+# 演習3-2 エラーバージョン
+
+# 12_sum = 2 + 1
+# sum_two_one
+# print(sum_two_one)
+
+# if = 2 + 1
+# if
+# print(if)
