c83fc688e6db6dc428ad714ee68e90d945c4eb10
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-09-19 19:26:15 +0900

    :+1: #3 演習3-6

--- p3-1.py
+++ p3-1.py
@@ -1,7 +1,7 @@
 # x の平方根を求める 
 x = 2
 #
-rnew = x
+rmew = x
 #
 r1 = rnew
 r2 = x/r1 # 三角形の関係式から図形的に求めている
@@ -28,3 +28,9 @@ print(r1, rnew, r2)
 # 1.5 1.4166666666666665 1.3333333333333333
 # 1.4166666666666665 1.4142156862745097 1.411764705882353
 # 1.4142156862745097 1.4142135623746899 1.41421143847487
+
+# Traceback (most recent call last):
+#   File "/**/KURENAI_Python_Tutorial/p3-1.py", line 6, in <module>
+#     r1 = rnew
+#          ^^^^
+# NameError: name 'rnew' is not defined. Did you mean: 'rmew'?
