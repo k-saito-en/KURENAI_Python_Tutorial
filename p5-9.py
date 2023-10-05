4b8f2b534e417210b77599618ca961eeb0c8537e
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-10-05 21:38:30 +0900

    :+1: #5 演習5-10

--- p5-9.py
+++ p5-9.py
@@ -1,14 +1,24 @@
-for i in range(3):
-    for j in range(3):
-        print(i,j)
+# for i in range(3):
+#     for j in range(3):
+#         print(i,j)
+
+# # 出力結果
+# # 0 0
+# # 0 1
+# # 0 2
+# # 1 0
+# # 1 1
+# # 1 2
+# # 2 0
+# # 2 1
+# # 2 2
+
+a = [5, 1, 3, 4]
+sum = 0
+for i in range(len(a)):
+    sum += a[i]
+average = sum/len(a)
+print(average)
 
 # 出力結果
-# 0 0
-# 0 1
-# 0 2
-# 1 0
-# 1 1
-# 1 2
-# 2 0
-# 2 1
-# 2 2
+# 3.25
