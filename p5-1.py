a8b9ba35e0d16571973db1223032f660ae51f87d
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-10-05 20:37:37 +0900

    :+1: #5 演習5-5

--- p5-1.py
+++ p5-1.py
@@ -3,8 +3,12 @@ x = 2
 #
 rnew = x
 #
-for i in range(1000000)
+for i in range(1000000):
     r1 = rnew
-    r2 = x / r1
+      r2 = x / r1
     rnew = (r1 + r2)/ 2
     print(r1, rnew, r2)
+
+# 出力結果
+#     r2 = x / r1
+# IndentationError: unexpected indent
