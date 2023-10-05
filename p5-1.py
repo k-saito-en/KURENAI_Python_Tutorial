21787ae60daf0635517c70062f3cdd0f172fe818
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-10-05 20:31:32 +0900

    :+1: #5 演習5-4

--- p5-1.py
+++ p5-1.py
@@ -3,11 +3,8 @@ x = 2
 #
 rnew = x
 #
-for i in range(1000000):
+for i in range(1000000)
     r1 = rnew
     r2 = x / r1
     rnew = (r1 + r2)/ 2
     print(r1, rnew, r2)
-
-# 出力結果
-# 1.414213562373095 1.414213562373095 1.4142135623730951
