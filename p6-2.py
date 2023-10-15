129e64ab0f2a0f32511f337fd17dd30da9dc4b48
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-10-15 11:23:15 +0900

    :+1: #6 演習6-1

---
+++ p6-2.py
@@ -0,0 +1,15 @@
+# 交差点名を格納する空の二重配列を作成しておく
+cross_table = [["", "", ""], ["", "", ""], ["", "", ""]]
+# 材料を用意
+tozai = ["三条", "四条", "五条"]
+nanboku = ["堀川", "鳥丸", "河原町"]
+# for文で格納していく
+for i in range(len(tozai)):
+    for j in range(len(nanboku)):
+        cross = tozai[i] + nanboku[j]
+        cross_table[i][j] = cross
+
+print(cross_table)
+
+# 出力結果
+# [['三条堀川', '三条鳥丸', '三条河原町'], ['四条堀川', '四条鳥丸', '四条河原町'], ['五条堀川', '五条鳥丸', '五条河原町']]
