ceca2260f6eac8d25c2b4ec1ee64672e115b6ecf
k-saito-en <saito.kotaro.engineer@gmail.com>
2023-10-21 12:43:07 +0900

    :+1: #7 演習7-4

--- p7-3.py
+++ p7-3.py
@@ -1,17 +1,23 @@
 #
+def get_positive_numeral(element1, element2):
+    diff = element1 - element2
+    if diff < 0:
+        return -diff
+    else:
+        return diff
+
+
 def square_root(x):
     "引数 x の平方根を求める"
     rnew = x
     #
-    diff = rnew - x / rnew
-    if diff < 0:
-        diff = -diff
+    diff = get_positive_numeral(rnew, x / rnew)
     while (diff > 1.0E-6):
         r1 = rnew
         r2 = x / r1
         rnew = (r1 + r2) / 2
         print(r1, rnew, r2)
-        diff = r1 - r2
+        diff = get_positive_numeral(r1, r2)
         if diff < 0:
             diff = -diff
     return rnew
